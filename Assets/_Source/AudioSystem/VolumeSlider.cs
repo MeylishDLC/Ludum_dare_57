fe3b9647b2aaa197441674c2c477021c7c5a29a0
MeylishDLC <meylishy20@gmail.com>
2025-04-07 21:06:11 +0300

    feat: main menu

---
+++ Assets/_Source/AudioSystem/VolumeSlider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace AudioSystem
+{
+    public class VolumeSlider: MonoBehaviour
+    {
+        private enum VolumeType
+        {
+            MASTER,
+            MUSIC,
+            SFX
+        }
+
+        [Header("Type")] 
+        [SerializeField] private VolumeType volumeType;
+
+        private Slider _volumeSlider;
+        private SoundManager _soundManager;
+
+        [Inject]
+        public void Initialize(SoundManager soundManager)
+        {
+            _soundManager = soundManager;
+        }
+        private void Start()
+        {
+            if (_soundManager == null)
+            {
+                _soundManager = FindObjectOfType<SoundManager>();
+            }
+            _volumeSlider = GetComponent<Slider>();
+            _volumeSlider.onValueChanged.AddListener(_ => OnSliderValueChanged());
+            
+            switch (volumeType)
+            {
+                case VolumeType.MASTER:
+                    _volumeSlider.value = _soundManager.MasterVolume;
+                    break;
+                case VolumeType.SFX:
+                    _volumeSlider.value = _soundManager.SFXVolume;
+                    break;
+                case VolumeType.MUSIC:
+                    _volumeSlider.value = _soundManager.MusicVolume;
+                    break;
+                default:
+                    throw new Exception($"Volume type not supported: {volumeType}");
+            }
+        }
+        private void OnSliderValueChanged()
+        {
+            switch (volumeType)
+            {
+                case VolumeType.MASTER:
+                    _soundManager.MasterVolume = _volumeSlider.value;
+                    break;
+                case VolumeType.SFX:
+                    _soundManager.SFXVolume = _volumeSlider.value;
+                    break;
+                case VolumeType.MUSIC:
+                    _soundManager.MusicVolume = _volumeSlider.value;
+                    break;
+                default:
+                    throw new Exception($"Volume is not supported {volumeType}");
+            }
+        }
+    }
+}
---
+++ Assets/_Source/UI/Menu/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AudioSystem;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI.Menu
+{
+    public class MainMenu: MonoBehaviour
+    {
+        [SerializeField] private Button startGameButton;
+        [SerializeField] private Button settingsButton;
+        
+        [Header("Settings")]
+        [SerializeField] private Button returnButton;
+        [SerializeField] private RectTransform settingsScreen;
+        
+        private SoundManager _soundManager;
+
+        [Inject]
+        public void Initialize(SoundManager soundManager)
+        {
+            _soundManager = soundManager;
+        }
+        private void Start()
+        {
+            startGameButton.onClick.AddListener(LoadGameScene);
+            settingsButton.onClick.AddListener(OpenSettingsScreen);
+            returnButton.onClick.AddListener(CloseSettingsScreen);
+            
+            _soundManager.InitializeMusic(_soundManager.FMODEvents.MainMenuMusic);
+        }
+        private void LoadGameScene()
+        {
+            _soundManager.CleanUp();
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+        private void OpenSettingsScreen()
+        {
+            settingsScreen.gameObject.SetActive(true);
+        }
+        private void CloseSettingsScreen()
+        {
+            settingsScreen.gameObject.SetActive(false);
+        }
+    }
+}
